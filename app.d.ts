381d72f5fdabe008c13aaca86524e2f517b6ba3f
BibeKJodd <bibekjodd@outlook.com>
2023-05-25 13:23:13 +0545

    feat User type added on Express Request

--- app.d.ts
+++ app.d.ts
@@ -1,10 +1,16 @@
 import { z } from "zod";
 import { envVariables } from "./src/config/appConfig";
+import { IUser } from "./src/models/User";
 export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv extends z.infer<typeof envVariables> {
       NODE_ENV: "production" | "development";
+    }
+  }
+  namespace Express {
+    interface Request {
+      user: IUser;
     }
   }
   var envLoaded: boolean;
