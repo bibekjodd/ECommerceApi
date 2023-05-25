b6d6546bc3771bf3435052ffdcde75c7a2bec49b
BibeKJodd <bibekjodd@outlook.com>
2023-05-25 15:49:28 +0545

    user and admin verification middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,10 +3,6 @@ import { ErrorHandler } from "../utils/errorHandler";
 import { catchAsyncError } from "./catchAsyncError";
 import jwt from "jsonwebtoken";
 
-/**
- * checks if the user is authenticated or not from
- * `req.cookies.token`
- */
 export const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
   try {
     const token = req.cookies?.token;
@@ -16,7 +12,7 @@ export const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
       );
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET) as { id: string };
-    if (!decoded?.id)
+    if (!decoded.id)
       return next(
         new ErrorHandler("Please login to access this resource", 401)
       );
@@ -26,19 +22,21 @@ export const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
       return next(
         new ErrorHandler("Please login to access this resource", 401)
       );
+
+    req.user = user;
+    next();
   } catch (error) {
     return next(new ErrorHandler("Please login to access this resource", 401));
   }
-
-  next();
 });
 
 /**
- * admin validation middleware
- * `isAuthenticatedUser` must be used before this
+ * middleware that checks if the user is admin or not
+ * isAuthenticatedUser must be used before this.
  */
 export const isAdmin = catchAsyncError(async (req, res, next) => {
-  if (req.user.role !== "admin")
+  if (req.user?.role !== "admin")
     return next(new ErrorHandler("Only admin can perform this action", 403));
+
   next();
 });
