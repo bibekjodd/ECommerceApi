37888c1bd1d06247f1ed20ca7971e5bc5d7f5223
bibekjodd <bibekjodd@outlook.com>
2024-02-10 15:45:44 +0545

    add notification module

--- src/routes/notification.route.ts
+++ src/routes/notification.route.ts
@@ -1,7 +1,8 @@
+import { getNotifications } from '@/controllers/notification.controller';
 import { isAuthenticatedUser } from '@/middlewares/auth';
 import { Router } from 'express';
 
 const router = Router();
 export const notificationRoute = router;
 
-router.get('/notifications', isAuthenticatedUser);
+router.get('/notifications', isAuthenticatedUser, getNotifications);
